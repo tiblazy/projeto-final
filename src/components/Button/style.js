b35368cf03d06fb5556a37f159481f7f8fffe26a
KAeHM <samuelsclima03@gmail.com>
2022-07-06 02:28:28 -0300

    feat: Adicionando components reutilizaveis; button e input

---
+++ src/components/Button/style.js
@@ -0,0 +1,15 @@
+import styled from "styled-components";
+
+export const ButtonComponent = styled.button`
+  height: 40px;
+  width: ${(props) => props.width || "223px"};
+
+  font-size: 1rem;
+  color: white;
+
+  border: none;
+  border-radius: 5px;
+  cursor: pointer;
+
+  background-color: #7e0902;
+`;
