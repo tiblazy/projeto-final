53b92a6221a79679c591e938bddf1d770f5ae26f
João Gonçalves <joaogoncalves@MacBook-Air-de-Joao.local>
2022-07-12 20:46:50 -0300

    estilizacao pre pronta das pages de register e login

--- src/components/Pages/Register/index.js
+++ src/components/Pages/Register/index.js
@@ -6,7 +6,7 @@ import { useNavigate } from "react-router-dom";
 import { yupResolver } from "@hookform/resolvers/yup";
 import { schemaRegister } from "../../../validators/yup";
 
-import { Background, Container } from "./style";
+import { Container,Form,Background,Cards, TextField } from "./style";
 import { InputComponent } from "../../Input/style";
 import { ButtonComponent } from "../../Button/style";
 
@@ -48,40 +48,54 @@ export const Register = () => {
 
   return (
     <>
-      <Background>
         <Container>
+            <Background>
+            </Background>
+
+            <Form>
+          <Cards>
           <h1> Cadastro </h1>
           <form onSubmit={handleSubmit(onRegisterFunction)}>
+            <TextField>
             <InputComponent
               label="Nome"
               placeholder="insira seu usuário"
               {...register("name")}
             />
             <p>{errors.name?.message}</p>
+            </TextField>
+            <TextField>
             <InputComponent
               label="Email"
               placeholder="insira seu email"
               {...register("email")}
             />
             <p>{errors.email?.message}</p>
+            </TextField>
+            <TextField>
             <InputComponent
               label="password"
               placeholder="insira seu password"
               {...register("password")}
             />
             <p>{errors.password?.message}</p>
+            </TextField>
+            <TextField>
             <InputComponent
               label="confirme sua senha"
               placeholder="confirme sua senha"
               {...register("confirmPassword")}
             />
             <p>{errors.confirmPassword?.message}</p>
+           
             <ButtonComponent>Cadastrar</ButtonComponent>
+            </TextField>
             Já possui cadastro? clique
-            <a onClick={() => navigate(login)}>aqui</a> para fazer login
+            <a onClick={() => navigate(login)}> aqui </a> para fazer login
           </form>
+          </Cards>
+          </Form>
         </Container>
-      </Background>
     </>
   );
 };
